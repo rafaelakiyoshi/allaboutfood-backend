4a9f7ea5ab80af7b91ad520c648836c2a4109ee8
Rafael Akiyoshi <rafah94@hotmail.com>
2021-02-03 18:24:37 -0500

    removing useless code

--- src/index.js
+++ src/index.js
@@ -1,13 +1,10 @@
-import { ApolloServer, gql } from "apollo-server";
+import { ApolloServer } from "apollo-server";
 import resolvers from "./resolvers";
 import typeDefs from "./typeDefs";
 import { connect } from "./config/mongoConnect";
 connect();
 
-const server = new ApolloServer({
-  typeDefs,
-  resolvers,
-});
+const server = new ApolloServer({ typeDefs, resolvers });
 
 server.listen({ port: 4000 }, () =>
   console.log(`🚀 Server ready at http://localhost:4000${server.graphqlPath}`)
