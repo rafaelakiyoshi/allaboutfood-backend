266c7823680c80ec157bd5a82ce661ce758b9692
Rafael Akiyoshi <rafah94@hotmail.com>
2021-02-03 18:25:10 -0500

    updating typedefs

--- src/typeDefs.js
+++ src/typeDefs.js
@@ -10,9 +10,43 @@ export default gql`
     favoriteRestaurants: [String]
   }
 
+  type Comment {
+    user: ID!
+    text: String!
+  }
+
+  enum FoodType {
+    PIZZA
+    FASTFOOD
+    SUSHI
+    HEALTHY
+    LAMEN
+    GREEK
+    ASIATIC
+    BRAZILIAN
+    MEXICAN
+    ITALIAN
+    DESSERT
+    BAKERY
+    BRUNCH
+    COFFEE
+  }
+
+  type Restaurant {
+    id: ID!
+    name: String!
+    type: [FoodType!]!
+    score: Float
+    photos: [String]!
+    comments: [Comment]!
+    city: String!
+  }
+  
   type Query {
     getUser(id: ID!): User
     getUsers: [User]!
+    getRestaurant(id: ID!): Restaurant
+    getRestaurants: [Restaurant]!
   }
 
   input NewUser {
@@ -22,7 +56,15 @@ export default gql`
     lastName: String
   }
 
+  input NewRestaurant {
+    name: String!
+    photos: [String]
+    type: [String!]!
+    city: String!
+  }
+
   type Mutation {
     createUser(input: NewUser!): User!
+    createRestaurant(input: NewRestaurant!): Restaurant!
   }
 `
