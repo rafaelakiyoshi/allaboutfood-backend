402cc64e7d820f3691267123b07712401606ae61
Rafael Akiyoshi <rafah94@hotmail.com>
2021-02-03 17:40:08 -0500

    adding first API structure

---
+++ src/index.js
@@ -0,0 +1,14 @@
+import { ApolloServer, gql } from "apollo-server";
+import resolvers from "./resolvers";
+import typeDefs from "./typeDefs";
+import { connect } from "./config/mongoConnect";
+connect();
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+});
+
+server.listen({ port: 4000 }, () =>
+  console.log(`🚀 Server ready at http://localhost:4000${server.graphqlPath}`)
+);
